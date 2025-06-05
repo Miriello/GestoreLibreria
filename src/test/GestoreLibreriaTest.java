fc41cb379a8252548906bdc5d71861e5e149f008
Francesco Miriello <f.miriello98@gmail.com>
2025-06-05 12:11:57 +0200

    Introduzione di nuovi test

--- src/test/GestoreLibreriaTest.java
+++ src/test/GestoreLibreriaTest.java
@@ -14,43 +14,62 @@ public class GestoreLibreriaTest {
     public void inizio() {
         gestoreLibri = new GestoreLibriArray();
         gestoreLibri.add(new Libro("La coscienza di Zeno", "Italo Svevo", 00001, "Romanzo", null, StatoLettura.DALEGGERE));
-        gestoreLibri.add(new Libro("Novecento","Alessandro Baricco",00002,"Teatrale",Valutazione.CINQUE,StatoLettura.LETTO));
+        gestoreLibri.add(new Libro("Novecento", "Alessandro Baricco", 00002, "Teatrale", Valutazione.CINQUE, StatoLettura.LETTO));
     }
+
     @Test
-    public void testAdd(){
+    public void testAdd() {
         assertEquals(2, gestoreLibri.size());
-        gestoreLibri.add(new Libro("Il giovane Holden","J.D.Salinger",00003,"Romanzo",null,StatoLettura.DALEGGERE));
+        gestoreLibri.add(new Libro("Il giovane Holden", "J.D.Salinger", 00003, "Romanzo", null, StatoLettura.DALEGGERE));
         assertEquals(3, gestoreLibri.size());
     }
+
     @Test
-    public void testGetAutore(){
+    public void testGetAutore() {
         Libro l = gestoreLibri.getLibro("La coscienza di Zeno");
         assertEquals("Italo Svevo", l.getAutore());
     }
 
     @Test
-    public void testGetLibro(){
+    public void testGetLibro() {
         Libro l = gestoreLibri.getLibro("La coscienza di Zeno");
         assertEquals("La coscienza di Zeno", l.getTitolo());
     }
+
     @Test
-    public void testRemove(){
+    public void testRemove() {
         assertEquals(2, gestoreLibri.size());
         gestoreLibri.remove(gestoreLibri.getLibro("La coscienza di Zeno"));
         assertEquals(1, gestoreLibri.size());
     }
+
     @Test
-    public void testFiltroAutore(){
+    public void testFiltroAutore() {
         GestoreLibri temp = new GestoreLibriArray();
         temp = gestoreLibri.filtroAutore("Italo Svevo");
         Libro l = temp.getLibro("La coscienza di Zeno");
         assertEquals("Italo Svevo", l.getAutore());
     }
+
     @Test
-    public void testFiltroGenere(){
+    public void testFiltroGenere() {
         GestoreLibri temp = new GestoreLibriArray();
         temp = gestoreLibri.filtroGenere("Romanzo");
         Libro l = temp.getLibro("La coscienza di Zeno");
         assertEquals("Romanzo", l.getGenere());
+    }
+    @Test
+    public void testFiltroStatoLettura() {
+        GestoreLibri temp = new GestoreLibriArray();
+        temp = gestoreLibri.filtroStatoLettura(StatoLettura.LETTO);
+        Libro l = temp.getLibro("Novecento");
+        assertEquals(StatoLettura.LETTO, l.getStatoLettura());
+    }
+    @Test
+    public void testOrdinaPerTitolo() {
+        GestoreLibri temp = new GestoreLibriArray();
+        temp = gestoreLibri.ordinaPerTitolo(gestoreLibri);
+        Libro l = temp.getLibro("La coscienza di Zeno");
+        assertEquals("La coscienza di Zeno", l.getTitolo());
     }
 }
