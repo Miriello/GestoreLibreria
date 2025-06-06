38eb4441b92098a1b52605bc4d0c0b475ca81452
Francesco Miriello <f.miriello98@gmail.com>
2025-06-06 14:21:10 +0200

    Risolta la struttura del bridge. Introduzione di una nuova classe LibreriaAbstract

---
+++ src/LibreriaAbstract.java
@@ -0,0 +1,65 @@
+import database.*;
+import gestore.*;
+import libro.*;
+
+
+public abstract class LibreriaAbstract {
+    protected GestoreFile gestoreFile;
+    protected GestoreLibri gestoreLibri;
+
+    public LibreriaAbstract(GestoreFile gestoreFile, GestoreLibri gestoreLibri) {
+        this.gestoreFile = gestoreFile;
+        this.gestoreLibri = gestoreLibri;
+    }
+
+    public void add(Libro libro){
+        gestoreLibri.add(libro);
+    }
+    public void remove(Libro libro){
+        gestoreLibri.remove(libro);
+    }
+    public Libro getLibro(String titolo){
+        return gestoreLibri.getLibro(titolo);
+    }
+    public int size(){
+        return gestoreLibri.size();
+    }
+    public String toString(){
+        return gestoreLibri.toString();
+    }
+    // MODIFICA LO STATO DELL'ISTANZA
+    public GestoreLibri ordinaPerTitolo(){
+        return gestoreLibri = gestoreLibri.ordinaPerTitolo();
+    }
+    //MODIFICA LO STATO DELL'ISTANZA
+    public GestoreLibri ordinaPerAutore(){
+        return gestoreLibri = gestoreLibri.ordinaPerAutore();
+    }
+    //RESTITUISCE UNA RAPPRESENTAZIONE TEMPORANEA
+    public GestoreLibri filtroAutore(String autore){
+        return gestoreLibri.filtroAutore(autore);
+    }
+    //RESTITUISCE UNA RAPPRESENTAZIONE TEMPORANEA
+    public GestoreLibri filtroGenere(String genere){
+        return gestoreLibri.filtroGenere(genere);
+    }
+    //RESTITUISCE UNA RAPPRESENTAZIONE TEMPORANEA
+    public GestoreLibri filtroStatoLettura(StatoLettura stato){
+        return gestoreLibri.filtroStatoLettura(stato);
+    }
+    public void salvaFile(String percorso){
+        try {
+            gestoreFile.salvaFile(gestoreLibri, percorso);
+        } catch (Exception e) {
+            System.err.println("Errore durante il salvataggio: " + e.getMessage());
+        }
+    }
+    public GestoreLibri caricaFile(String percorso){
+        try {
+            return gestoreFile.caricaFile(percorso);
+        } catch (Exception e) {
+            System.err.println("Errore durante il caricamento: " + e.getMessage());
+            return null;
+        }
+    }
+}
