48ed43fc744401ca98f15f06585121329a82b9c5
Francesco Miriello <f.miriello98@gmail.com>
2025-06-06 18:40:36 +0200

    La GUI prende forma, con le funzioni di ordinamento per titolo e per autore, aggiunge inoltre le funzioni di aggiunta libro e rimozione

--- src/GUI/LibreriaGUI.java
+++ src/GUI/LibreriaGUI.java
@@ -8,10 +8,13 @@ import gestore.*;
 import libreria.Libreria;
 import libro.*;
 
+import java.awt.event.ActionListener;
+
 public class LibreriaGUI extends JFrame{
     private Libreria libreria;
     private JTable tabellaLibri;
     private DefaultTableModel tableModel;
+    private ActionListener listener;
 
     public LibreriaGUI(Libreria libreria){
         this.libreria = libreria;
@@ -29,15 +32,24 @@ public class LibreriaGUI extends JFrame{
         //FILE > CARICA / SALVA
         JMenu menuFile = new JMenu("File");
         JMenuItem menuItemSalva = new JMenuItem("Salva");
-        JMenuItem menuItemCarica = new JMenuItem("Carica");
+        JMenuItem menuItemSalvaConNome= new JMenuItem("Salva con nome");
+        JMenuItem menuItemApri = new JMenuItem("Apri");
+        menuItemSalva.addActionListener(listener);
+        menuItemSalvaConNome.addActionListener(listener);
+        menuItemApri.addActionListener(listener);
+        menuItemSalva.addActionListener(listener);
         barraMenu.add(menuFile);
+        menuFile.add(menuItemApri);
         menuFile.add(menuItemSalva);
-        menuFile.add(menuItemCarica);
+        menuFile.add(menuItemSalvaConNome);
+
 
         //OPZIONI > AGGIUNGI / RIMUOVI
         JMenu menuOpzioni = new JMenu("Opzioni");
         JMenuItem menuItemAggiungi = new JMenuItem("Aggiungi");
+        menuItemAggiungi.addActionListener(e -> aggiungiLibro());
         JMenuItem menuItemRimuovi = new JMenuItem("Rimuovi");
+        menuItemRimuovi.addActionListener(e -> rimuoviLibro());
         barraMenu.add(menuOpzioni);
         menuOpzioni.add(menuItemAggiungi);
         menuOpzioni.add(menuItemRimuovi);
@@ -45,28 +57,77 @@ public class LibreriaGUI extends JFrame{
         //ORDINA > PER TITOLO / PER AUTORE
         JMenu menuOrdina = new JMenu("Ordina");
         JMenuItem menuItemOrdinaTitolo = new JMenuItem("Per Titolo");
+        menuItemOrdinaTitolo.addActionListener(e -> {
+            libreria.ordinaPerTitolo();
+            aggiornaTabella();
+        });
         JMenuItem menuItemOrdinaAutore = new JMenuItem("Per Autore");
+        menuItemOrdinaAutore.addActionListener(e -> {
+            libreria.ordinaPerAutore();
+            aggiornaTabella();
+        });
         barraMenu.add(menuOrdina);
         menuOrdina.add(menuItemOrdinaTitolo);
         menuOrdina.add(menuItemOrdinaAutore);
+
+        //CREDITI
+        JMenu menuCrediti = new JMenu("Crediti");
+        menuCrediti.addActionListener(e -> mostraCrediti());
+        barraMenu.add(menuCrediti);
 
         setJMenuBar(barraMenu);
 
         //TABELLA
         String[] campi = {"Titolo", "Autore", "Genere", "Codice ISBN", "Valutazione", "StatoLettura"};
-        tableModel = new DefaultTableModel(campi, 0);
+        tableModel = new DefaultTableModel(campi, 0){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         tabellaLibri = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(tabellaLibri);
         add(scrollPane);
     }
     public void aggiornaTabella(){
+        tableModel.setRowCount(0);
         for(Libro l : libreria.getLibri()){
             Object[] riga = {l.getTitolo(), l.getAutore(), l.getGenere(), l.getCodiceISBN(), l.getValutazionePersonale(), l.getStatoLettura()};
             tableModel.addRow(riga);
         }
 
     }
+    public void mostraCrediti(){
+        JFrame crediti = new JFrame("Crediti");
+        setSize(400,400);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        setVisible(true);
+    }
 
+    public void aggiungiLibro(){
+        String titolo = JOptionPane.showInputDialog("Inserisci il titolo del libro");
+        String autore = JOptionPane.showInputDialog("Inserisci l'autore del libro");
+        int codiceISBN = Integer.parseInt(JOptionPane.showInputDialog("Inserisci il codice ISBN del libro"));
+        String genere = JOptionPane.showInputDialog("Inserisci il genere del libro");
+        Valutazione valutazione = Valutazione.valueOf(JOptionPane.showInputDialog("Inserisci la valutazione del libro"));
+        StatoLettura stato = StatoLettura.valueOf(JOptionPane.showInputDialog("Inserisci lo stato della lettura del libro"));
+        Libro libro = new Libro(titolo, autore, codiceISBN, genere, valutazione, stato);
+        libreria.add(libro);
+        aggiornaTabella();
+    }
+
+    public void rimuoviLibro(){
+        int rigaSelezionata = tabellaLibri.getSelectedRow();
+        if(rigaSelezionata != -1){
+            String titolo = (String) tabellaLibri.getValueAt(rigaSelezionata, 0);
+            String autore = (String) tabellaLibri.getValueAt(rigaSelezionata, 1);
+            int codiceISBN = (int) tabellaLibri.getValueAt(rigaSelezionata, 2);
+            String genere = (String) tabellaLibri.getValueAt(rigaSelezionata, 3);
+            Valutazione valutazione = Valutazione.valueOf((String) tabellaLibri.getValueAt(rigaSelezionata, 4));
+            StatoLettura stato = StatoLettura.valueOf((String) tabellaLibri.getValueAt(rigaSelezionata, 5));
+            Libro libro = new Libro(titolo, autore, codiceISBN, genere, valutazione, stato);
+        }
+    }
 
 }
 
